8047bb07f9bd69cf02b13a1609b3d5990820aa10
Omkesh Narwade <70096943+omkesh360@users.noreply.github.com>
2026-02-08 16:50:29 +0530

    update 1.0
    new commit 4.50 pn=m by omkesh360

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'travelhub-v2';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './stays.html',
+    './car-rentals.html',
+    './bike-rentals.html',
+    './restaurant.html',
+    './attraction.html',
+    './bus.html',
+    './images/travelhub-logo.png',
+    './images/hero_csn.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
